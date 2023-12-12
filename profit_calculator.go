359595f9e082b77762312e82f9c318baa30f691d
Petar Cevriz <pcevriz95@gmail.com>
2023-12-12 18:51:34 +0100

    Use functions to wrap the functionality

--- profit_calculator.go
+++ profit_calculator.go
@@ -4,23 +4,27 @@ import (
 	"fmt"
 )
 
-func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+func getInputFromUser(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
-
+func calculateFinancials(revenue float64, expenses float64, taxRate float64) (float64, float64, float64) {
 	earningsBeforeTaxes := revenue - expenses
 	profit := earningsBeforeTaxes - (earningsBeforeTaxes * taxRate)
 	ratio := profit / revenue
+
+	return earningsBeforeTaxes, profit, ratio
+}
+
+func main() {
+	revenue := getInputFromUser("Revenue: ")
+	expenses := getInputFromUser("Expenses: ")
+	taxRate := getInputFromUser("Tax rate: ")
+
+	earningsBeforeTaxes, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Println("EBT: ", earningsBeforeTaxes)
 	fmt.Println("Profit: ", profit)
