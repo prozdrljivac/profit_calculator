34845491d5b39bc78beb0136178001649741cc1e
Petar Cevriz <pcevriz95@gmail.com>
2023-12-15 14:45:23 +0100

    Add check for input values and way to store results

--- profit_calculator.go
+++ profit_calculator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func getInputFromUser(infoText string) float64 {
@@ -19,14 +20,25 @@ func calculateFinancials(revenue float64, expenses float64, taxRate float64) (fl
 	return earningsBeforeTaxes, profit, ratio
 }
 
+func storeResults(earningsBeforeTaxes float64, profit float64, ratio float64) {
+	results := fmt.Sprint("Earnings before taxes: ", earningsBeforeTaxes, "\nProfit: ", profit, "\nRatio: ", ratio)
+	os.WriteFile("financials.txt", []byte(results), os.FileMode(0644))
+}
+
 func main() {
 	revenue := getInputFromUser("Revenue: ")
 	expenses := getInputFromUser("Expenses: ")
 	taxRate := getInputFromUser("Tax rate: ")
+
+	if revenue <= 0 || expenses <= 0 || taxRate <= 0 {
+		fmt.Println("Invalid input, all values must be greater than 0.")
+		return
+	}
 
 	earningsBeforeTaxes, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Println("EBT: ", earningsBeforeTaxes)
 	fmt.Println("Profit: ", profit)
 	fmt.Println("Ratio: ", ratio)
+	storeResults(earningsBeforeTaxes, profit, ratio)
 }
